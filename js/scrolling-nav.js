9152e15f35fef4d4fe0e5623d5aa54cf0ee5a85e
jnjco86 <john.cxt@gmail.com>
2020-09-15 05:54:08 +0800

    added new videos

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +16,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -27,3 +27,30 @@
   });
 
 })(jQuery); // End of use strict
+
+(function () {
+  var qs, js, q, s, d = document,
+    gi = d.getElementById,
+    ce = d.createElement,
+    gt = d.getElementsByTagName,
+    id = "typef_orm",
+    b = "https://embed.typeform.com/";
+  if (!gi.call(d, id)) {
+    js = ce.call(d, "script");
+    js.id = id;
+    js.src = b + "embed.js";
+    q = gt.call(d, "script")[0];
+    q.parentNode.insertBefore(js, q)
+  }
+})()
+
+$('#v-pills-tab a').on('click', function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+$('#v-pills-tab a[href="#v-pills-1"]').tab('show') // Select tab by name
+$('#v-pills-tab li:#v-pills-2 a').tab('show') // Select first tab
+$('#v-pills-tab li:#v-pills-3 a').tab('show') // Select last tab
+$('#v-pills-tab li:#v-pills-4 a').tab('show') // Select third tab
+$('#v-pills-tab li:#v-pills-5 a').tab('show')
